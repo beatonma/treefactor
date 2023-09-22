b338f69ba09d91ca3f8fc16ad1ddaa968f09e7b3
Michael Beaton <beatonma@gmail.com>
2023-09-22 17:15:34 +0100

    Refactored drag + drop.

--- src/tree/ui.tsx
+++ src/tree/ui.tsx
@@ -3,119 +3,58 @@ import { Tree, TreeDirectory, TreeFile, TreeNode, TreeNodeType } from "./data";
 import { OptionsContext } from "./options";
 
 type OnTreeChange = (update: (prev: Tree) => Tree) => void;
-const TreeChangeContext = createContext<OnTreeChange | undefined>(undefined);
+const OnTreeChangeContext = createContext<OnTreeChange | undefined>(undefined);
 
 interface NodeProps {
-  editable: boolean;
+  isEditable: boolean;
 }
 export const TreeUI = (props: {
   tree: Tree;
   onEdit?: (update: (prev: Tree) => Tree) => void;
 }) => {
   const { tree, onEdit } = props;
+  const droppableProps = DragDrop.dropTargetProps(tree.fullPath, onEdit);
   const editable = onEdit !== undefined;
 
-  const onDrop = (ev: DragEvent) => {
-    ev.preventDefault();
-    const data = JSON.parse(
-      ev.dataTransfer.getData(DragDropKey),
-    ) as DragDropPayload;
-    ev.dataTransfer.clearData();
-
-    onEdit?.((prev) => {
-      const tree = prev.clone();
-      tree.move(data.fullPath, tree.fullPath);
-      return tree;
-    });
-  };
-
   return (
-    <TreeChangeContext.Provider value={onEdit}>
-      <div
-        className="tree"
-        data-editable={editable}
-        onDrop={onDrop}
-        onDragOver={enableDragDrop(editable)}
-      >
+    <OnTreeChangeContext.Provider value={onEdit}>
+      <div className="tree" data-editable={editable} {...droppableProps}>
         <Name name={tree.name} />
-        <Contents children={tree.children} editable={editable} />
+        <Contents children={tree.children} isEditable={editable} />
       </div>
-    </TreeChangeContext.Provider>
+    </OnTreeChangeContext.Provider>
   );
 };
 
-const DragDropKey = "node";
-interface DragDropPayload {
-  type: TreeNodeType;
-  fullPath: string;
-}
-const toDragDropPayload = (node: TreeNode): DragDropPayload => {
-  if (node instanceof TreeDirectory)
-    return {
-      type: "directory",
-      fullPath: node.fullPath,
-    };
-  return {
-    type: "file",
-    fullPath: node.fullPath,
-  };
-};
-const startDrag = (node: TreeNode) => (ev: DragEvent) => {
-  ev.stopPropagation();
-  const payload = toDragDropPayload(node);
-  ev.dataTransfer.setData(DragDropKey, JSON.stringify(payload));
-};
-
-const enableDragDrop = (enabled: boolean) => {
-  if (enabled)
-    return (ev: DragEvent) => {
-      ev.preventDefault();
-    };
-};
-
 const DirectoryUI = (props: { directory: TreeDirectory } & NodeProps) => {
-  const { directory, editable } = props;
-  const onTreeChange = useContext(TreeChangeContext);
-
-  const onDrop = (ev: DragEvent) => {
-    ev.preventDefault();
-    const data = JSON.parse(
-      ev.dataTransfer.getData(DragDropKey),
-    ) as DragDropPayload;
-    ev.dataTransfer.clearData();
-
-    onTreeChange?.((prev) => {
-      const tree = prev.clone();
-      tree.move(data.fullPath, directory.fullPath);
-      return tree;
-    });
-  };
+  const onTreeChange = useContext(OnTreeChangeContext);
+  const { directory, isEditable } = props;
+  const draggableProps = DragDrop.draggableSourceProps(isEditable, directory);
+  const droppableProps = DragDrop.dropTargetProps(
+    directory.fullPath,
+    onTreeChange,
+  );
 
   return (
     <div
       className="directory"
-      draggable={editable}
-      onDragStart={startDrag(directory)}
-      onDragOver={enableDragDrop(editable)}
-      onDrop={onDrop}
       title={directory.path}
+      {...draggableProps}
+      {...droppableProps}
     >
       <Name name={directory.name} />
-      <Contents children={directory.children} editable={editable} />
+      <Contents children={directory.children} isEditable={isEditable} />
     </div>
   );
 };
 
 const FileUI = (props: { file: TreeFile } & NodeProps) => {
-  const { file, editable } = props;
+  const { file, isEditable } = props;
+
+  const draggableProps = DragDrop.draggableSourceProps(isEditable, file);
 
   return (
-    <div
-      className="file"
-      draggable={editable}
-      onDragStart={startDrag(file)}
-      title={file.fullPath}
-    >
+    <div className="file" title={file.fullPath} {...draggableProps}>
       <Name name={file.name} />
     </div>
   );
@@ -128,7 +67,7 @@ const Name = (props: { name: string }) => {
 
 const Contents = (props: { children: TreeNode[] } & NodeProps) => {
   const options = useContext(OptionsContext);
-  const { children, editable } = props;
+  const { children, isEditable } = props;
 
   const renderableContents = options.showFiles
     ? children
@@ -142,13 +81,13 @@ const Contents = (props: { children: TreeNode[] } & NodeProps) => {
             <DirectoryUI
               key={child.fullPath}
               directory={child}
-              editable={editable}
+              isEditable={isEditable}
             />
           );
 
         if (child instanceof TreeFile) {
           return (
-            <FileUI key={child.fullPath} file={child} editable={editable} />
+            <FileUI key={child.fullPath} file={child} isEditable={isEditable} />
           );
         }
 
@@ -178,3 +117,67 @@ const summarizeContents = (
 
   return [...dirs, ...fileTypes];
 };
+
+namespace DragDrop {
+  const DragDropKey = "node";
+
+  interface DragDropPayload {
+    type: TreeNodeType;
+    fullPath: string;
+  }
+
+  const toDragDropPayload = (node: TreeNode): DragDropPayload => {
+    if (node instanceof TreeDirectory)
+      return {
+        type: "directory",
+        fullPath: node.fullPath,
+      };
+    return {
+      type: "file",
+      fullPath: node.fullPath,
+    };
+  };
+  export const draggableSourceProps = (
+    isDraggable: boolean,
+    payload: TreeNode,
+  ) => {
+    return {
+      draggable: isDraggable,
+      onDragStart: (ev: DragEvent) => {
+        ev.stopPropagation();
+        const encoded = toDragDropPayload(payload);
+        ev.dataTransfer.setData(DragDropKey, JSON.stringify(encoded));
+      },
+    };
+  };
+
+  export const dropTargetProps = (
+    targetPath: string,
+    onTreeChange: OnTreeChange | undefined,
+  ) => {
+    if (onTreeChange === undefined) {
+      return {};
+    }
+
+    return {
+      onDragOver: (ev: DragEvent) => {
+        ev.preventDefault();
+      },
+
+      onDrop: (ev: DragEvent) => {
+        ev.preventDefault();
+        const data = JSON.parse(
+          ev.dataTransfer.getData(DragDropKey),
+        ) as DragDropPayload;
+        console.log(`onDrop ${data.type} ${data.fullPath}`);
+        ev.dataTransfer.clearData();
+
+        onTreeChange((prev) => {
+          const tree = prev.clone();
+          tree.move(data.fullPath, targetPath);
+          return tree;
+        });
+      },
+    };
+  };
+}
