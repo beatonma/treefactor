efcc724d135e551c343aa78035e8c97d3f6ae8cf
Michael Beaton <beatonma@gmail.com>
2023-09-25 15:24:21 +0100

    Minor refactor.

--- src/tree/parse.ts
+++ src/tree/parse.ts
@@ -7,30 +7,39 @@ import {
   TreeNodeType,
 } from "./data";
 
+export const parseTree = (data: string): Tree => {
+  return parseTreeJson(data) ?? new Tree("empty root", []);
+};
+
 interface TreeJson {
   type: TreeNodeType;
   name: string;
   contents?: TreeJson[];
 }
+
 /**
  *
- * @param tree Tree JSON data as printed from the `tree -J` shell utility.
+ * @param json Tree JSON data as printed from the `tree -J` shell utility.
  */
-export const parseTree = (json: string): Tree => {
+const parseTreeJson = (json: string): Tree | undefined => {
   const parseBranch = (path: string, branch: TreeJson): TreeNode => {
     switch (branch.type) {
       case "directory":
-        return new TreeDirectory(path, branch.name, () =>
-          branch.contents!.map((it) =>
-            parseBranch(Path.joinPath(path, branch.name), it),
-          ),
+        return new TreeDirectory(
+          path,
+          branch.name,
+          () =>
+            branch.contents?.map((it) =>
+              parseBranch(Path.joinPath(path, branch.name), it),
+            ) ?? [],
         );
       case "file":
         return new TreeFile(path, branch.name);
     }
   };
 
-  const root: TreeJson = JSON.parse(json)[0];
+  const raw = JSON.parse(json);
+  const root: TreeJson = raw[0];
   const children = root.contents!.map((it) => parseBranch(root.name, it));
   return new Tree(root.name, children);
 };
